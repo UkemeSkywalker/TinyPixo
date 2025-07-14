4700ae44e2b097114969bb79f862a6532d71ee71
UkemeSkywalker <skywalkershadow9@gmail.com>
2025-07-15 00:43:44 +0100

    fixed vips-cpp issues

--- test-docker.sh
+++ test-docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Building Docker image..."
-docker build -t tinypixo-test .
+docker build -t tinypixo-test:v1.2.0 .
 
 echo "Running Docker container..."
-docker run -p 3000:3000 tinypixo-test
+docker run -p 3000:3000 tinypixo-test:v1.2.0
 
 echo "Access the app at http://localhost:3000"
