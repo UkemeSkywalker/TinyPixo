5bee87db5caba9e6f11b5351f351faf0dbae56f9
UkemeSkywalker <skywalkershadow9@gmail.com>
2025-11-23 22:18:42 +0100

    update navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -12,6 +12,7 @@ export default function Navigation() {
     { href: "/", label: "🖼️ Image Optimizer" },
     // { href: '/audio-converter', label: '🎵 Audio Converter' },
     // { href: '/video-converter', label: '🎬 Video Converter' },
+    { href: '/pdf-converter', label: '📄 PDF Compressor' },
   ];
 
   return (
